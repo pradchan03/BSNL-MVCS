dad47c0aaa93b2063e7d8e70c45f2fbba71a0d60
MY PC <pradchan3@gmail.com>
2023-07-16 11:39:49 +0530

    meeting confimation

--- src/pages/ScheduleConfirmation.tsx
+++ src/pages/ScheduleConfirmation.tsx
@@ -1,9 +1,117 @@
-import { IonText } from "@ionic/react"
-
+import {
+  IonText,
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonBackButton,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonLabel,
+} from '@ionic/react';
+import './ScheduleConfirmation.scss';
 const ScheduleConfirmation: React.FC = () => {
-    return (
-        <IonText>Meeting has been Scheduled</IonText>
-    )
-}
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonBackButton defaultHref="/tabs/schedule"></IonBackButton>
+          </IonButtons>
+          <IonTitle>Metting Confirmation</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonCard className="custom-card">
+          <IonCardHeader>
+            <IonCardTitle>Conference created successfully!</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>
+            The conference notification has been sent to you via SMS or email.
+          </IonCardContent>
+        </IonCard>
+        <div>
+          <IonGrid>
+            <IonRow>
+              <IonCol size="4">
+                <IonLabel className="account-labels"> Subject </IonLabel>
+              </IonCol>
+              <IonCol size="8">
+                <IonText style={{ fontWeight: '600' }}>fjskfs</IonText>
+              </IonCol>
+              {/* <IonCol size="4">
+                  <IonLabel className="account-labels"> Date </IonLabel>
+                </IonCol>
+                <IonCol size="8">
+                  <IonText style={{ fontWeight: '600' }}>date here </IonText>
+                </IonCol>
+                <IonCol size="4">
+                  <IonLabel className="account-labels"> Start time </IonLabel>
+                </IonCol>
+                <IonCol size="8">
+                  <IonText style={{ fontWeight: '600' }}>start time </IonText>
+                </IonCol>
+                <IonCol size="4">
+                  <IonLabel className="account-labels">
+                    {' '}
+                    Conference ID{' '}
+                  </IonLabel>
+                </IonCol>
+                <IonCol size="8">
+                  <IonText style={{ fontWeight: '600' }}>ID </IonText>
+                </IonCol>
+                <IonCol size="4">
+                  <IonLabel className="account-labels">
+                    {' '}
+                    Access number{' '}
+                  </IonLabel>
+                </IonCol>
+                <IonCol size="8">
+                  <IonText style={{ fontWeight: '600' }}>
+                    Access Number{' '}
+                  </IonText>
+                </IonCol>
+                <IonCol size="4">
+                  <IonLabel className="account-labels">
+                    {' '}
+                    Chairperson Password{' '}
+                  </IonLabel>
+                </IonCol>
+                <IonCol size="8">
+                  <IonText style={{ fontWeight: '600' }}>
+                    chairperson password{' '}
+                  </IonText>
+                </IonCol>
+                <IonCol size="4">
+                  <IonLabel className="account-labels">
+                    {' '}
+                    Guest Password{' '}
+                  </IonLabel>
+                </IonCol>
+                <IonCol size="8">
+                  <IonText style={{ fontWeight: '600' }}>
+                    guest password{' '}
+                  </IonText>
+                </IonCol>
+                <IonCol size="4">
+                  <IonLabel className="account-labels"> Creator </IonLabel>
+                </IonCol>
+                <IonCol size="8">
+                  <IonText style={{ fontWeight: '600' }}>Admin </IonText>
+                </IonCol> */}
+            </IonRow>
+          </IonGrid>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
+};
 
 export default ScheduleConfirmation;
